39b3fb7a9c9cb9d96c612ca8f5d2385360081fab
jfaith630 <jfaith630@gmail.com>
2020-01-21 14:47:48 -0500

    Checkpoint-17-jQuery-Player-Bar-Buttons: Completed

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -16,6 +16,7 @@
     $('#song-list').append(song.element);
     song.element.on('click', event => {
       player.playPause(song);
+      $('button#play-pause').attr('playState', player.playState);
     })
   });
 }
